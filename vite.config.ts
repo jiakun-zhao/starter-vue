91ea2abef0456c71ddcdd7e5675cbad68251b30d
Wythe <50269993+unnb@users.noreply.github.com>
2023-01-11 19:32:23 +0800

    chore: init

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { URL, fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+import Vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+    plugins: [Vue()],
+    resolve: {
+        alias: {
+            '~/': fileURLToPath(new URL('./src', import.meta.url)),
+        },
+    },
+})
