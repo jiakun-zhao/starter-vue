bf8b57e30abe5076efe16c190faf535746e7eba3
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-02-22 01:59:22 +0800

    feat: nprogress

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,21 @@
 import { createApp } from 'vue'
 import { RouterView, createRouter, createWebHistory } from 'vue-router'
+import NProgress from 'nprogress'
 
 import routes from '~pages'
 
 import '~/assets/index.css'
+import 'nprogress/nprogress.css'
 import 'uno.css'
 
+const router = createRouter({ history: createWebHistory(), routes })
+NProgress.configure({ showSpinner: false })
+router.beforeEach(async (to, from, next) => {
+    NProgress.start()
+    next()
+})
+router.afterEach(() => NProgress.done())
+
 const app = createApp(RouterView)
-app.use(createRouter({ history: createWebHistory(), routes }))
+app.use(router)
 app.mount('#app')
