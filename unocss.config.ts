0746b2e33b35cea8789c4067c88d301923e41443
Jiakun Zhao <jiakun.zhao@outlook.com>
2023-03-15 13:11:52 +0800

    chore: split unocss config

---
+++ unocss.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
+
+export default defineConfig({
+    presets: [
+        presetUno({
+            dark: 'media',
+            preflight: false,
+        }),
+        presetAttributify(),
+        presetIcons({
+            scale: 1.2,
+            extraProperties: { 'display': 'inline-block', 'vertical-align': 'bottom' },
+        }),
+    ],
+})
--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,17 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import JsDelivr from 'vite-plugin-jsdelivr'
 import UnoCSS from 'unocss/vite'
-import { presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
+    plugins: [
+        Vue(),
+        JsDelivr(),
+        UnoCSS(),
+    ],
     resolve: {
         alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) },
     },
     server: {
         host: true,
     },
-    plugins: [
-        Vue(),
-        JsDelivr(),
-        UnoCSS({
-            presets: [
-                presetUno({
-                    dark: 'media',
-                    preflight: false,
-                }),
-                presetAttributify(),
-                presetIcons({
-                    scale: 1.2,
-                    extraProperties: { 'display': 'inline-block', 'vertical-align': 'bottom' },
-                }),
-            ],
-        }),
-    ],
 })
