5735029c1f74ca6c228cea4352805866bc1bdac4
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-07-10 20:09:50 +0800

    chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -2,19 +2,17 @@ import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 
 import Vue from '@vitejs/plugin-vue'
-// import JsDelivr from 'vite-plugin-jsdelivr'
 import UnoCSS from 'unocss/vite'
 
 export default defineConfig({
-    plugins: [
-        Vue(),
-        // JsDelivr(),
-        UnoCSS(),
-    ],
-    resolve: {
-        alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) },
-    },
-    server: {
-        host: true,
-    },
+  plugins: [
+    Vue(),
+    UnoCSS(),
+  ],
+  resolve: {
+    alias: { '~': fileURLToPath(new URL('./src', import.meta.url)) },
+  },
+  server: {
+    host: true,
+  },
 })
