66b0b9bb0ec59c673b41af81cd685389c5b1270f
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2023-11-16 17:43:41 +0800

    chore: update

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,5 @@ import App from '~/App.vue'
 
 import 'uno.css'
 
-const app = createApp(App)
-app.mount('#app')
+createApp(App)
+  .mount('#app')
--- uno.config.ts
+++ uno.config.ts
@@ -2,15 +2,9 @@ import { defineConfig, presetAttributify, presetIcons, presetUno } from 'unocss'
 
 export default defineConfig({
   presets: [
-    presetUno({
-      dark: 'media',
-      preflight: false,
-    }),
+    presetUno({ dark: 'media' }),
     presetAttributify(),
-    presetIcons({
-      scale: 1.2,
-      extraProperties: { 'display': 'inline-block', 'vertical-align': 'bottom' },
-    }),
+    presetIcons({ scale: 1.2, extraProperties: { 'display': 'inline-block', 'vertical-align': 'bottom' } }),
   ],
   theme: {
     colors: {
