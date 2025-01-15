16307d2aeb6e86defe51f5e6ff91315e119b063f
Jiakun Zhao <50269993+jiakun-zhao@users.noreply.github.com>
2025-01-15 10:48:31 +0800

    chore: update deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,16 @@
+/* eslint perfectionist/sort-objects: "error" */
+
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-  stylistic: {
-    overrides: {
-      'no-console': 'warn',
-      'style/brace-style': ['warn', '1tbs'],
-      'ts/ban-ts-comment': 'off',
-      'ts/no-unused-expressions': 'off',
-      'unused-imports/no-unused-imports': 'warn',
-      'style/jsx-quotes': ['warn', 'prefer-single'],
-      'style/jsx-one-expression-per-line': 'off',
-    },
+  rules: {
+    'no-console': 'warn',
+    'style/brace-style': ['warn', '1tbs'],
+    'style/jsx-one-expression-per-line': 'off',
+    'style/jsx-quotes': ['warn', 'prefer-single'],
+    'ts/ban-ts-comment': 'off',
+    'ts/no-unused-expressions': 'off',
+    'unused-imports/no-unused-imports': 'warn',
   },
   vue: {
     overrides: {
